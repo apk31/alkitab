46feefa6e7bf7aa435d5b7ec3087377b3a589a1f
apk31 <44232314+apk31@users.noreply.github.com>
2026-03-21 14:52:20 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
 // otomatis trigger update cache.
 //
 // const BUILD_TIME  = '__BUILD_TIME__'; // diganti otomatis oleh generate_alkitab.py
-const BUILD_TIME = '20260322-1407';
+const BUILD_TIME = '20260322-1452';
 const CACHE_DATA  = 'alkitab-data-v1.1.0';          // JSON ayat (jarang berubah)
 const CACHE_SHELL = 'alkitab-shell-' + BUILD_TIME; // shell app (fresh tiap deploy)
 
